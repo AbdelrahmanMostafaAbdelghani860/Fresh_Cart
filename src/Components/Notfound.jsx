ff81dce7aee9946cd76b6f0974c70bc8ff0d38a3
Abdelrahman Moustafa <abdo99792@gmail.com>
2023-10-09 18:22:34 +0300

    Changes in not found

--- src/Components/Notfound.jsx
+++ src/Components/Notfound.jsx
@@ -4,8 +4,8 @@ export default function Notfound() {
   return (
     <div className="container">
       <div className="my-5 text-center mx-auto w-75 min-vh-100 ">
-        <h1 className="tect-center bg-main">Not found </h1>
-        <img src={img} alt="" className="my-4" />
+        <h1 className="tect-center">Not found </h1>
+        <img src={img} alt="" className="my-4 w-100" />
       </div>
     </div>
   );
