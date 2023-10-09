1834c20903c39a392cc57623c51fd3fae9fb3fb0
Abdelrahman Moustafa <abdo99792@gmail.com>
2023-10-09 17:44:23 +0300

    new\

--- src/Components/Notfound.jsx
+++ src/Components/Notfound.jsx
@@ -3,7 +3,7 @@ import img from "../assets/error.svg";
 export default function Notfound() {
   return (
     <div className="my-5 text-center mx-auto w-75 min-vh-100 ">
-      <h1 className="tect-center">Not found</h1>
+      <h1 className="tect-center bg-main">Not found </h1>
       <img src={img} alt="" className="my-4" />
     </div>
   );
