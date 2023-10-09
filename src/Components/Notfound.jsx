e6db6fd5280a7b407695de22bcbf32c8b288e564
Abdelrahman Moustafa <abdo99792@gmail.com>
2023-10-09 16:55:44 +0300

    Changes in footer

--- src/Components/Notfound.jsx
+++ src/Components/Notfound.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import img from "../assets/error.svg";
 export default function Notfound() {
   return (
-    <div className="my-5 text-center mx-auto w-75 ">
+    <div className="my-5 text-center mx-auto w-75 min-vh-100 ">
       <h1 className="tect-center">Not Found</h1>
       <img src={img} alt="" className="my-4" />
     </div>
