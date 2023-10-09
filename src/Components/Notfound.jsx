c7aa56bc797858936f41c721df8890f7e4139f0a
Abdelrahman Moustafa <abdo99792@gmail.com>
2023-10-09 18:01:14 +0300

    add

--- src/Components/Notfound.jsx
+++ src/Components/Notfound.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import img from "../assets/error.svg";
 export default function Notfound() {
   return (
-    <div className="my-5 text-center mx-auto w-75 min-vh-100 ">
-      <h1 className="tect-center bg-main">Not found </h1>
-      <img src={img} alt="" className="my-4" />
+    <div className="container">
+      <div className="my-5 text-center mx-auto w-75 min-vh-100 ">
+        <h1 className="tect-center bg-main">Not found </h1>
+        <img src={img} alt="" className="my-4" />
+      </div>
     </div>
   );
 }
